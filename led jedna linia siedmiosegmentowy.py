e3ceb75a670af36b42b8fd8d6776c758a965ed46
przemyslaw-junior <78381232+przemyslaw-junior@users.noreply.github.com>
2021-02-20 17:11:38 +0100

    Add files via upload

---
+++ led jedna linia siedmiosegmentowy.py
@@ -0,0 +1,131 @@
+print('Oto wyświetlacz led w twojej konsoli.\nPodaj liczbe a ja ją wyświetle')
+liczba = input ()
+obr = 0
+
+while obr < 9:
+    for i in liczba:
+        if obr == 0:
+            if i == '0':
+                print ('###',end=' ')
+            elif i == '1':
+                print ('  #',end=' ')
+            elif i == '2':
+                print ('###',end=' ')
+            elif i == '3':
+                print ('###',end=' ')
+            elif i == '4':
+                print ('# #',end=' ')
+            elif i == '5':
+                print ('###',end=' ')
+            elif i == '6':
+                print ('###',end=' ')
+            elif i == '7':
+                print ('###',end=' ')
+            elif i == '8':
+                print ('###',end=' ')
+            elif i == '9':
+                print ('###',end=' ')
+
+        if obr == 1:
+            print()
+            obr += 1
+
+        if obr == 2:
+            if i == '0':
+                print ('# #',end=' ')
+            elif i == '1':
+                print ('  #',end=' ')
+            elif i == '2':
+                print ('  #',end=' ')
+            elif i == '3':
+                print ('  #',end=' ')
+            elif i == '4':
+                print ('# #',end=' ')
+            elif i == '5':
+                print ('#  ',end=' ')
+            elif i == '6':
+                print ('#  ',end=' ')
+            elif i == '7':
+                print ('  #',end=' ')
+            elif i == '8':
+                print ('# #',end=' ')
+            elif i == '9':
+                print ('# #',end=' ')
+        if obr == 3:
+                print()
+                obr += 1
+
+        if obr == 4:
+            if i == '0':
+                print ('# #',end=' ')
+            elif i == '1':
+                print ('  #',end=' ')
+            elif i == '2':
+                print ('###',end=' ')
+            elif i == '3':
+                print ('###',end=' ')
+            elif i == '4':
+                print ('###',end=' ')
+            elif i == '5':
+                print ('###',end=' ')
+            elif i == '6':
+                print ('###',end=' ')
+            elif i == '7':
+                print ('  #',end=' ')
+            elif i == '8':
+                print ('###',end=' ')
+            elif i == '9':
+                print ('###',end=' ')
+
+        if obr == 5:
+            print()
+            obr += 1
+
+        if obr == 6:
+            if i == '0':
+                print ('# #',end=' ')
+            elif i == '1':
+                print ('  #',end=' ')
+            elif i == '2':
+                print ('#  ',end=' ')
+            elif i == '3':
+                print ('  #',end=' ')
+            elif i == '4':
+                print ('  #',end=' ')
+            elif i == '5':
+                print ('  #',end=' ')
+            elif i == '6':
+                print ('# #',end=' ')
+            elif i == '7':
+                print ('  #',end=' ')
+            elif i == '8':
+                print ('# #',end=' ')
+            elif i == '9':
+                print ('  #',end=' ')
+
+        if obr == 7:
+            print()
+            obr += 1
+
+        if obr == 8:
+            if i == '0':
+                print ('###',end=' ')
+            elif i == '1':
+                print ('  #',end=' ')
+            elif i == '2':
+                print ('###',end=' ')
+            elif i == '3':
+                print ('###',end=' ')
+            elif i == '4':
+                print ('  #',end=' ')
+            elif i == '5':
+                    print ('###',end=' ')
+            elif i == '6':
+                print ('###',end=' ')
+            elif i == '7':
+                print ('  #',end=' ')
+            elif i == '8':
+                print ('###',end=' ')
+            elif i == '9':
+                print ('###',end=' ')
+    obr +=1
